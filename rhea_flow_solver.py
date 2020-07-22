fb9a8d254072cf6c59a107761da3c8a95c4b615c
ProjectRHEA <project.rhea.contact@gmail.com>
2020-07-22 11:56:16 +0000

    Update rhea_flow_solver.py

--- rhea_flow_solver.py
+++ rhea_flow_solver.py
@@ -2,7 +2,7 @@
 
 ################################################################################################################
 #                                                                                                              #
-# RHEA - the open-source Reproducible Hybrid-architecture flow solver Engineered for Academia                  #
+# RHEA - an open-source Reproducible Hybrid-architecture flow solver Engineered for Academia                  #
 #                                                                                                              #
 # Rhea was the Titaness great Mother of the Gods, and goddess of female fertility, motherhood, and generation. #
 # Her name means "flow" and "ease", representing the eternal flow of time and generations with ease.           #
