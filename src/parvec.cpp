686eefbf9074394519babb51b55cf180e2e79d63
ProjectRHEA <project.rhea.contact@gmail.com>
2020-07-27 13:54:29 +0000

    Replace parvec.cpp

--- src/parvec.cpp
+++ src/parvec.cpp
@@ -14,6 +14,21 @@ parvec :: parvec(comm_scheme* topo)
     mydomain = topo;   
 }
 
+void parvec::setTopology(comm_scheme* topo) {
+
+    size = topo->getSize();
+    vector = new double[size];
+    ini_x = topo->iter_common[_INNER_][_INIX_] ;
+    ini_y = topo->iter_common[_INNER_][_INIY_] ;
+    ini_z = topo->iter_common[_INNER_][_INIZ_] ;
+    fin_x = topo->iter_common[_INNER_][_ENDX_] ;
+    fin_y = topo->iter_common[_INNER_][_ENDY_] ;
+    fin_z = topo->iter_common[_INNER_][_ENDZ_] ;
+
+    mydomain = topo;   
+
+}
+
 void parvec::update()
 {
     mydomain->update(vector);
