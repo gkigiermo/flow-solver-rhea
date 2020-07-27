043143693319dfcd0add4aeb833c6597c4e8bffd
ProjectRHEA <project.rhea.contact@gmail.com>
2020-07-27 18:01:19 +0000

    Update FlowSolverRHEA.hpp

--- FlowSolverRHEA.hpp
+++ FlowSolverRHEA.hpp
@@ -1,35 +1,3 @@
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                                              //
-// RHEA - an open-source Reproducible Hybrid-architecture flow solver Engineered for Academia                   //
-//                                                                                                              //
-// Rhea was the Titaness great Mother of the Gods, and goddess of female fertility, motherhood, and generation. //
-// Her name means "flow" and "ease", representing the eternal flow of time and generations with ease.           //
-//                                                                                                              //
-//                                                                                                              //
-// REHA is released under the MIT License:                                                                      //
-//                                                                                                              //
-// Copyright (c) 2020 Lluis Jofre Cruanyes & Guillermo Oyarzun Altamirano.                                      //
-//                                                                                                              //
-// Permission is hereby granted, free of charge, to any person obtaining a copy                                 //
-// of this software and associated documentation files (the "Software"), to deal                                //
-// in the Software without restriction, including without limitation the rights                                 //
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell                                    //
-// copies of the Software, and to permit persons to whom the Software is                                        //
-// furnished to do so, subject to the following conditions:                                                     //
-//                                                                                                              //
-// The above copyright notice and this permission notice shall be included in all                               //
-// copies or substantial portions of the Software.                                                              //
-//                                                                                                              //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR                                   //
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,                                     //
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE                                  //
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER                                       //
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,                                //
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE                                //
-// SOFTWARE.                                                                                                    //
-//                                                                                                              //
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 #ifndef _RHEA_FLOW_SOLVER_
 #define _RHEA_FLOW_SOLVER_
 
