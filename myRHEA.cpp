c0348a0dc4787c857a3d52d8418b599fae048f5f
lluis.jofre <lluis.jofre@upc.upc.edu>
2020-08-23 22:24:17 +0200

    Commit before testing lid-driven cavity

--- myRHEA.cpp
+++ myRHEA.cpp
@@ -2,17 +2,13 @@
 
 using namespace std;
 
-/// Pi number
-const double Pi = 2.0*asin( 1.0 );
-
 /// PROBLEM PARAMETERS ///
-const double gamma_0 = 1.4;					/// Reference ratio of heat capacities
-const double Re_0    = Pi;					/// Reynolds number
-const double Ma_0    = ( 1.0e-2 )/sqrt( gamma_0 );		/// Mach number
-const double rho_0   = 1.0;					/// Reference density	
-const double U_0     = 1.0;					/// Reference velocity
-const double mu_0    = rho_0*U_0*Pi/Re_0;			/// Dynamic viscosity	
-const double P_0     = ( rho_0/gamma_0 )*pow( U_0/Ma_0, 2.0 );	/// Reference pressure
+const double Re_L    = 100.0;				/// Reynolds number
+const double L       = 1.0;				/// Cavity size
+const double rho_ref = 1.0;				/// Reference density	
+const double P_ref   = 101325.0;			/// Reference pressure
+const double u_l     = 1.0;				/// Lid velocity
+const double nu      = u_l*L/Re_L;			/// Kinematic viscosity	
 
 ////////// myRHEA CLASS //////////
 
@@ -24,11 +20,11 @@ void myRHEA::setInitialConditions() {
     for(int i = topo->iter_common[_ALL_][_INIX_]; i <= topo->iter_common[_ALL_][_ENDX_]; i++) {
         for(int j = topo->iter_common[_ALL_][_INIY_]; j <= topo->iter_common[_ALL_][_ENDY_]; j++) {
             for(int k = topo->iter_common[_ALL_][_INIZ_]; k <= topo->iter_common[_ALL_][_ENDZ_]; k++) {
-                u_field[I1D(i,j,k)] = U_0*( sin( mesh->x[i] )*cos( mesh->y[j] ) );
-                v_field[I1D(i,j,k)] = ( -1.0 )*U_0*( cos( mesh->x[i] )*sin( mesh->y[j] ) );
+                u_field[I1D(i,j,k)] = 0.0;
+                v_field[I1D(i,j,k)] = 0.0;
                 w_field[I1D(i,j,k)] = 0.0;
-                P_field[I1D(i,j,k)] = P_0 - ( rho_0*U_0*U_0/4.0 )*( cos( 2.0*mesh->x[i] ) + cos( 2.0*mesh->y[j] ) );
-                T_field[I1D(i,j,k)] = P_field[I1D(i,j,k)]/( rho_0*R_specific );
+                P_field[I1D(i,j,k)] = P_ref;
+                T_field[I1D(i,j,k)] = P_field[I1D(i,j,k)]/( rho_ref*R_specific );
             }
         }
     }
