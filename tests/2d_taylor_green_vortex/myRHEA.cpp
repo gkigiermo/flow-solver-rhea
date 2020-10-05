91af247f73f2faa10f2206bce8232ae9f4550528
lluis.jofre <lluis.jofre@upc.edu>
2020-10-06 01:57:15 +0200

    Initial verification taylor-green vortex

--- tests/2d_taylor_green_vortex/myRHEA.cpp
+++ tests/2d_taylor_green_vortex/myRHEA.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-/// Pi number
-//const double Pi = 2.0*asin( 1.0 );
+/// pi number
+const double pi = 2.0*asin( 1.0 );
 
 /// PROBLEM PARAMETERS ///
 const double gamma_0 = 1.4;					/// Reference ratio of heat capacities
@@ -13,6 +13,7 @@ const double rho_0   = 1.0;					/// Reference density
 const double U_0     = 1.0;					/// Reference velocity
 //const double mu_0    = rho_0*U_0*Pi/Re_0;			/// Dynamic viscosity	
 const double P_0     = ( rho_0/gamma_0 )*pow( U_0/Ma_0, 2.0 );	/// Reference pressure
+const double L       = 2.0*pi;					/// Domain size			
 
 ////////// myRHEA CLASS //////////
 
@@ -24,10 +25,10 @@ void myRHEA::setInitialConditions() {
     for(int i = topo->iter_common[_ALL_][_INIX_]; i <= topo->iter_common[_ALL_][_ENDX_]; i++) {
         for(int j = topo->iter_common[_ALL_][_INIY_]; j <= topo->iter_common[_ALL_][_ENDY_]; j++) {
             for(int k = topo->iter_common[_ALL_][_INIZ_]; k <= topo->iter_common[_ALL_][_ENDZ_]; k++) {
-                u_field[I1D(i,j,k)] = U_0*( sin( mesh->x[i] )*cos( mesh->y[j] ) );
-                v_field[I1D(i,j,k)] = ( -1.0 )*U_0*( cos( mesh->x[i] )*sin( mesh->y[j] ) );
+                u_field[I1D(i,j,k)] = U_0*( cos( 2.0*pi*mesh->x[i]/L )*sin( 2.0*pi*mesh->y[j]/L ) );
+                v_field[I1D(i,j,k)] = ( -1.0 )*U_0*( sin( 2.0*pi*mesh->x[i]/L )*cos( 2.0*pi*mesh->y[j]/L ) );
                 w_field[I1D(i,j,k)] = 0.0;
-                P_field[I1D(i,j,k)] = P_0 - ( rho_0*U_0*U_0/4.0 )*( cos( 2.0*mesh->x[i] ) + cos( 2.0*mesh->y[j] ) );
+                P_field[I1D(i,j,k)] = P_0 + ( rho_0*U_0*U_0/4.0 )*( cos( 4.0*pi*mesh->x[i]/L ) + cos( 4.0*pi*mesh->y[j]/L ) );
                 T_field[I1D(i,j,k)] = P_field[I1D(i,j,k)]/( rho_0*thermodynamics->getSpecificGasConstant() );
             }
         }
---
+++ tests/2d_taylor_green_vortex/post_process_script.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+import sys
+import os
+import numpy as np
+import math
+import h5py    
+
+
+### Open data file
+data_file = h5py.File( '2d_taylor_green_vortex_27.h5', 'r' )
+#list( data_file.keys() )
+x_data     = data_file['x'][0,:,:];     x_data     = np.asarray( x_data.flatten() )
+y_data     = data_file['y'][0,:,:];     y_data     = np.asarray( y_data.flatten() )
+z_data     = data_file['z'][0,:,:];     z_data     = np.asarray( z_data.flatten() )
+rho_data   = data_file['rho'][0,:,:];   rho_data   = np.asarray( rho_data.flatten() )
+u_data     = data_file['u'][0,:,:];     u_data     = np.asarray( u_data.flatten() )
+v_data     = data_file['v'][0,:,:];     v_data     = np.asarray( v_data.flatten() )
+w_data     = data_file['w'][0,:,:];     w_data     = np.asarray( w_data.flatten() )
+E_data     = data_file['E'][0,:,:];     E_data     = np.asarray( E_data.flatten() )
+P_data     = data_file['P'][0,:,:];     P_data     = np.asarray( P_data.flatten() )
+T_data     = data_file['T'][0,:,:];     T_data     = np.asarray( T_data.flatten() )
+sos_data   = data_file['sos'][0,:,:];   sos_data   = np.asarray( sos_data.flatten() )
+mu_data    = data_file['mu'][0,:,:];    mu_data    = np.asarray( mu_data.flatten() )
+kappa_data = data_file['kappa'][0,:,:]; kappa_data = np.asarray( kappa_data.flatten() )
+ke_data    = 0.5*( u_data*u_data + v_data*v_data + w_data*w_data ) 
+e_data     = E_data - ke_data
+
+
+### Set fixed parameters
+rho_0 = 1.0					# Reference density [kg/m3]
+mu    = 1.0					# Dynamic viscosity [Pa s]
+nu    = mu/rho_0				# Kinematic viscosity [m2/s]
+U_0   = 1.0					# Reference velocity [m/s]
+L     = 2.0*np.pi				# Domain size [m]
+gamma = 1.4;					# Ratio of heat capacities [-]
+Ma    = ( 1.0e-2 )/np.sqrt( gamma )		# Mach number [-]
+P_0   = ( rho_0/gamma )*( ( U_0/Ma )**2.0 )	# Reference pressure [Pa]
+time  = 0.01					# Final time [s] 
+
+
+### Calculate L2-norm relative errors between discrete and analytical solution
+u_L2_norm_error = 0.0
+u_L2_norm_den   = 0.0
+v_L2_norm_error = 0.0
+v_L2_norm_den   = 0.0
+for i in range( 0, len( x_data ) ):
+    if( ( x_data[i] > 0.0 ) and ( x_data[i] < ( 2.0*np.pi ) ) ):
+        if( ( y_data[i] > 0.0 ) and ( y_data[i] < ( 2.0*np.pi ) ) ):
+            ### Exact values
+            F = math.exp( ( -2.0 )*nu*time ) 
+            u_exact = U_0*np.cos( 2.0*np.pi*x_data[i]/L )*np.sin( 2.0*np.pi*y_data[i]/L )*F
+            v_exact = ( -1.0 )*U_0*np.sin( 2.0*np.pi*x_data[i]/L )*np.cos( 2.0*np.pi*y_data[i]/L )*F
+            ### L2-norm errors
+            u_L2_norm_error += ( u_exact - u_data[i] )**2.0
+            u_L2_norm_den   += u_exact**2.0
+            v_L2_norm_error += ( v_exact - v_data[i] )**2.0
+            v_L2_norm_den   += v_exact**2.0
+u_L2_norm_error = np.sqrt( u_L2_norm_error )/np.sqrt( u_L2_norm_den )
+v_L2_norm_error = np.sqrt( v_L2_norm_error )/np.sqrt( v_L2_norm_den )
+
+
+### Print results
+print( 'L2-norm relative errors:' + ' u_L2_norm = ' + str( u_L2_norm_error ) + ', v_L2_norm = ' + str( v_L2_norm_error ) )
