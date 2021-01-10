c39afe62302b152506b1e76d3476b6dcfa0225c2
Lluis Jofre <lluis.jofre@upc.edu>
2021-01-10 21:57:04 +0100

    Inflow-outflow test created

---
+++ tests/1d_inflow_outflow/myRHEA.cpp
@@ -0,0 +1,99 @@
+#include "myRHEA.hpp"
+
+using namespace std;
+
+/// PROBLEM PARAMETERS ///
+//const double R_specific = 287.058;				/// Specific gas constant
+const double gamma_0    = 1.4;					/// Heat capacity ratio
+//const double c_p        = gamma_0*R_specific/( gamma_0 - 1.0 );	/// Isobaric heat capacity
+const double Ma         = 3.0e-1;				/// Mach number
+//const double Pr         = 0.71;					/// Prandtl number
+const double rho_0      = 1.0;					/// Reference density	
+const double u_b        = 1.0;					/// Bulk velocity
+//const double mu         = 1.0;					/// Dynamic viscosity
+//const double kappa      = c_p*mu/Pr;				/// Thermal conductivity	
+const double P_0        = rho_0*u_b*u_b/( gamma_0*Ma*Ma );	/// Reference pressure
+
+////////// myRHEA CLASS //////////
+
+void myRHEA::setInitialConditions() {
+
+    /// IMPORTANT: This method needs to be modified/overwritten according to the problem under consideration
+
+    /// All (inner, halo, boundary): u, v, w, P and T
+    for(int i = topo->iter_common[_ALL_][_INIX_]; i <= topo->iter_common[_ALL_][_ENDX_]; i++) {
+        for(int j = topo->iter_common[_ALL_][_INIY_]; j <= topo->iter_common[_ALL_][_ENDY_]; j++) {
+            for(int k = topo->iter_common[_ALL_][_INIZ_]; k <= topo->iter_common[_ALL_][_ENDZ_]; k++) {
+                u_field[I1D(i,j,k)] = 0.0;
+                v_field[I1D(i,j,k)] = 0.0;
+                w_field[I1D(i,j,k)] = 0.0;
+                P_field[I1D(i,j,k)] = P_0;
+                T_field[I1D(i,j,k)] = P_field[I1D(i,j,k)]/( 1.0*thermodynamics->getSpecificGasConstant() );
+            }
+        }
+    }
+
+    /// Update halo values
+    u_field.update();
+    v_field.update();
+    w_field.update();
+    P_field.update();
+    T_field.update();
+
+};
+
+void myRHEA::calculateSourceTerms() {
+
+    /// IMPORTANT: This method needs to be modified/overwritten according to the problem under consideration
+
+    /// Inner points: f_rhou, f_rhov, f_rhow and f_rhoE
+    for(int i = topo->iter_common[_INNER_][_INIX_]; i <= topo->iter_common[_INNER_][_ENDX_]; i++) {
+        for(int j = topo->iter_common[_INNER_][_INIY_]; j <= topo->iter_common[_INNER_][_ENDY_]; j++) {
+            for(int k = topo->iter_common[_INNER_][_INIZ_]; k <= topo->iter_common[_INNER_][_ENDZ_]; k++) {
+                f_rhou_field[I1D(i,j,k)] = 0.0;
+                f_rhov_field[I1D(i,j,k)] = 0.0;
+                f_rhow_field[I1D(i,j,k)] = 0.0;
+                f_rhoE_field[I1D(i,j,k)] = 0.0;
+            }
+        }
+    }
+
+    /// Update halo values
+    //f_rhou_field.update();
+    //f_rhov_field.update();
+    //f_rhow_field.update();
+    //f_rhoE_field.update();
+
+};
+
+
+////////// MAIN //////////
+int main(int argc, char** argv) {
+
+    /// Initialize MPI
+    MPI_Init(&argc, &argv);
+
+    /// Process command line arguments
+    string configuration_file;
+    if( argc >= 2 ) {
+        configuration_file = argv[1];
+    } else {
+        cout << "Proper usage: RHEA.exe configuration_file.yaml" << endl;
+        MPI_Abort( MPI_COMM_WORLD, 1 );
+    }
+
+    /// Construct my RHEA
+    myRHEA my_RHEA( configuration_file );
+
+    /// Execute my RHEA
+    my_RHEA.execute();
+
+    /// Destruct my RHEA ... destructor is called automatically
+
+    /// Finalize MPI
+    MPI_Finalize();
+
+    /// Return exit code of program
+    return 0;
+
+}
---
+++ tests/1d_inflow_outflow/myRHEA.hpp
@@ -0,0 +1,33 @@
+#ifndef _MY_RHEA_
+#define _MY_RHEA_
+
+////////// INCLUDES //////////
+#include "../../src/FlowSolverRHEA.hpp"
+
+////////// NAMESPACES //////////
+using namespace std;
+
+////////// myRHEA CLASS //////////
+class myRHEA : public FlowSolverRHEA {
+   
+    public:
+
+        ////////// CONSTRUCTORS & DESTRUCTOR //////////
+        myRHEA(const string configuration_file) : FlowSolverRHEA(configuration_file) {};	/// Parametrized constructor
+        virtual ~myRHEA() {};									/// Destructor
+
+	////////// SOLVER METHODS //////////
+        
+        /// Set initial conditions: u, v, w, P and T ... needs to be modified/overwritten according to the problem under consideration
+        void setInitialConditions();
+
+        /// Calculate rhou, rhov, rhow and rhoE source terms ... needs to be modified/overwritten according to the problem under consideration
+        void calculateSourceTerms();
+
+    protected:
+
+    private:
+
+};
+
+#endif /*_MY_RHEA_*/
