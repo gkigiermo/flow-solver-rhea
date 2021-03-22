2432c87c987bc1a02d516f448cb366ee4baeb426
Lluis Jofre <lluis.jofre@upc.edu>
2021-03-22 14:14:40 +0100

    Test 2d_developing_plane_poiseuille_flow added

---
+++ tests/2d_developing_plane_poiseuille_flow/myRHEA.cpp
@@ -0,0 +1,110 @@
+#include "myRHEA.hpp"
+
+using namespace std;
+
+/// Pi number
+//const double pi = 2.0*asin( 1.0 );
+
+/// PROBLEM PARAMETERS ///
+//const double R_specific = 287.058;				/// Specific gas constant
+const double gamma_0    = 1.4;					/// Heat capacity ratio
+//const double c_p        = gamma_0*R_specific/( gamma_0 - 1.0 );	/// Isobaric heat capacity
+const double delta      = 1.0;					/// Channel half-height
+const double Re_tau     = 30.0; 				/// Friction Reynolds number
+const double Ma         = 3.0e-1;				/// Mach number
+//const double Pr         = 0.71;					/// Prandtl number
+const double rho_0      = 1.0;					/// Reference density	
+const double u_tau      = 1.0;					/// Friction velocity
+const double tau_w      = rho_0*u_tau*u_tau;			/// Wall shear stress
+const double mu         = rho_0*u_tau*delta/Re_tau;		/// Dynamic viscosity	
+const double nu         = u_tau*delta/Re_tau;			/// Kinematic viscosity	
+//const double kappa      = c_p*mu/Pr;				/// Thermal conductivity	
+const double Re_b       = pow( Re_tau/0.09, 1.0/0.88 );         /// Bulk (approximated) Reynolds number
+const double u_b        = nu*Re_b/( 2.0*delta );                /// Bulk (approximated) velocity
+const double P_0        = rho_0*u_b*u_b/( gamma_0*Ma*Ma );	/// Reference pressure
+
+////////// myRHEA CLASS //////////
+
+void myRHEA::setInitialConditions() {
+
+    /// IMPORTANT: This method needs to be modified/overwritten according to the problem under consideration
+
+    /// All (inner, halo, boundary): u, v, w, P and T
+    for(int i = topo->iter_common[_ALL_][_INIX_]; i <= topo->iter_common[_ALL_][_ENDX_]; i++) {
+        for(int j = topo->iter_common[_ALL_][_INIY_]; j <= topo->iter_common[_ALL_][_ENDY_]; j++) {
+            for(int k = topo->iter_common[_ALL_][_INIZ_]; k <= topo->iter_common[_ALL_][_ENDZ_]; k++) {
+                //u_field[I1D(i,j,k)] = ( ( tau_w/delta )/( 2.0*mu ) )*mesh->y[j]*( 2.0*delta - mesh->y[j] );
+                u_field[I1D(i,j,k)] = u_b;
+                v_field[I1D(i,j,k)] = 0.0;
+                w_field[I1D(i,j,k)] = 0.0;
+                P_field[I1D(i,j,k)] = P_0;
+                T_field[I1D(i,j,k)] = thermodynamics->calculateTemperatureFromPressureDensity( P_field[I1D(i,j,k)], rho_0 );
+            }
+        }
+    }
+
+    /// Update halo values
+    u_field.update();
+    v_field.update();
+    w_field.update();
+    P_field.update();
+    T_field.update();
+
+};
+
+void myRHEA::calculateSourceTerms() {
+
+    /// IMPORTANT: This method needs to be modified/overwritten according to the problem under consideration
+
+    /// Inner points: f_rhou, f_rhov, f_rhow and f_rhoE
+    for(int i = topo->iter_common[_INNER_][_INIX_]; i <= topo->iter_common[_INNER_][_ENDX_]; i++) {
+        for(int j = topo->iter_common[_INNER_][_INIY_]; j <= topo->iter_common[_INNER_][_ENDY_]; j++) {
+            for(int k = topo->iter_common[_INNER_][_INIZ_]; k <= topo->iter_common[_INNER_][_ENDZ_]; k++) {
+                //f_rhou_field[I1D(i,j,k)] = tau_w/delta;
+                f_rhou_field[I1D(i,j,k)] = 0.0;
+                f_rhov_field[I1D(i,j,k)] = 0.0;
+                f_rhow_field[I1D(i,j,k)] = 0.0;
+                f_rhoE_field[I1D(i,j,k)] = ( -1.0 )*( f_rhou_field[I1D(i,j,k)]*u_field[I1D(i,j,k)] + f_rhov_field[I1D(i,j,k)]*v_field[I1D(i,j,k)] + f_rhow_field[I1D(i,j,k)]*w_field[I1D(i,j,k)] );
+            }
+        }
+    }
+
+    /// Update halo values
+    //f_rhou_field.update();
+    //f_rhov_field.update();
+    //f_rhow_field.update();
+    //f_rhoE_field.update();
+
+};
+
+
+////////// MAIN //////////
+int main(int argc, char** argv) {
+
+    /// Initialize MPI
+    MPI_Init(&argc, &argv);
+
+    /// Process command line arguments
+    string configuration_file;
+    if( argc >= 2 ) {
+        configuration_file = argv[1];
+    } else {
+        cout << "Proper usage: RHEA.exe configuration_file.yaml" << endl;
+        MPI_Abort( MPI_COMM_WORLD, 1 );
+    }
+
+    /// Construct my RHEA
+    myRHEA my_RHEA( configuration_file );
+
+    /// Execute my RHEA
+    my_RHEA.execute();
+
+    /// Destruct my RHEA ... destructor is called automatically
+
+    /// Finalize MPI
+    MPI_Finalize();
+
+    /// Return exit code of program
+    return 0;
+
+}
---
+++ tests/2d_developing_plane_poiseuille_flow/post_process_plot_script.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+import sys
+import os
+import numpy as np
+import h5py    
+import matplotlib.pyplot as plt
+from matplotlib.transforms import Bbox
+from matplotlib import rc,rcParams
+#np.set_printoptions(threshold=sys.maxsize)
+plt.rc( 'text', usetex = True )
+rc('font', family='sanserif')
+plt.rc( 'font', size = 20 )
+plt.rcParams['text.latex.preamble'] = [ r'\usepackage{amsmath}', r'\usepackage{amssymb}', r'\usepackage{color}' ]
+
+
+### Open data file
+data_file = h5py.File( '2d_developing_plane_poiseuille_flow_341782.h5', 'r' )
+# Read data
+x_index    = 195
+x_data     = data_file['x'][1,:,x_index];     x_data     = np.asarray( x_data.flatten() )
+y_data     = data_file['y'][1,:,x_index];     y_data     = np.asarray( y_data.flatten() )
+z_data     = data_file['z'][1,:,x_index];     z_data     = np.asarray( z_data.flatten() )
+rho_data   = data_file['rho'][1,:,x_index];   rho_data   = np.asarray( rho_data.flatten() )
+u_data     = data_file['u'][1,:,x_index];     u_data     = np.asarray( u_data.flatten() )
+v_data     = data_file['v'][1,:,x_index];     v_data     = np.asarray( v_data.flatten() )
+w_data     = data_file['w'][1,:,x_index];     w_data     = np.asarray( w_data.flatten() )
+E_data     = data_file['E'][1,:,x_index];     E_data     = np.asarray( E_data.flatten() )
+P_data     = data_file['P'][1,:,x_index];     P_data     = np.asarray( P_data.flatten() )
+T_data     = data_file['T'][1,:,x_index];     T_data     = np.asarray( T_data.flatten() )
+sos_data   = data_file['sos'][1,:,x_index];   sos_data   = np.asarray( sos_data.flatten() )
+mu_data    = data_file['mu'][1,:,x_index];    mu_data    = np.asarray( mu_data.flatten() )
+kappa_data = data_file['kappa'][1,:,x_index]; kappa_data = np.asarray( kappa_data.flatten() )
+ke_data    = 0.5*( u_data*u_data + v_data*v_data + w_data*w_data )
+e_data     = E_data - ke_data
+
+
+### Theoretical plane Poiseuille velocity profile
+delta        = 1.0          # Channel half-height [m]
+u_tau        = 1.0          # Friction velocity [m/s]
+rho_0        = 1.0          # Reference density [kg/m3]
+mu           = 0.033333     # Dynamic viscosity [Pa s]
+nu           = mu/rho_0     # Kinematic viscosity [m2/s]
+G            = 1.0          # Constant pressure gradient [Pa/m]
+h            = 2.0*delta    # Channel height [m]
+num_y_points = 100          # Number of points in wall-normal direction
+
+y_points  = np.linspace( 0.0, h, num = num_y_points )
+u_profile = ( G/( 2.0*mu ) )*y_points*( h - y_points )
+u_b       = np.sum( u_profile )/num_y_points
+#print( u_b )
+
+
+### Plot u+ vs. y+
+
+# Clear plot
+plt.clf()
+
+# Read & Plot data
+plt.scatter( y_data*u_tau/nu, u_data/u_tau, marker = 'p', s = 50, color = 'firebrick', zorder = 1, label = r'$\textrm{RHEA}$' )
+plt.plot( y_points*u_tau/nu, u_profile/u_tau, linestyle = '-', linewidth = 1, color = 'black', zorder = 0, label = r'$\textrm{Exact}$' )
+
+# Configure plot
+plt.xlim( 0.0, 60.0 )
+plt.xticks( np.arange( 0.0, 60.01, 15.0 ) )
+plt.tick_params( axis = 'x', bottom = True, top = True, labelbottom = 'True', labeltop = 'False', direction = 'in' )
+#plt.xscale( 'log' )
+plt.xlabel( r'$y^{+}$' )
+plt.ylim( 0.0, 20.0 )
+plt.yticks( np.arange( 0.0, 20.1, 5.0 ) )
+plt.tick_params( axis = 'y', left = True, right = True, labelleft = 'True', labelright = 'False', direction = 'in' )
+#plt.yscale( 'log' )
+plt.ylabel( r'$u^{+}$' )
+legend = plt.legend( shadow = False, fancybox = False, frameon = False, loc='upper left' )
+plt.tick_params( axis = 'both', pad = 7.5 )
+plt.savefig( 'u_plus_vs_y_plus.eps', format = 'eps', bbox_inches = 'tight' )
