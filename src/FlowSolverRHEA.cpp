48f545738e201be5d91224342b66cf9d44a96082
Lluis Jofre <lluis.jofre@upc.edu>
2021-07-06 18:02:00 +0200

    Diffusive weight changed to 2

--- src/FlowSolverRHEA.cpp
+++ src/FlowSolverRHEA.cpp
@@ -2282,7 +2282,8 @@ double MurmanRoeLmFluxApproximateRiemannSolver::calculateIntercellFlux(const dou
     double delta_Ma_limit = 0.3;
     double Ma_L = abs( u_L/a_L ), Ma_R = abs( u_R/a_R );
     double delta_Ma = abs( Ma_L - Ma_R );
-    double phi = max( 0.0, sin( min( 1.0, delta_Ma/delta_Ma_limit )*0.5*pi ) );
+    //double phi = max( 0.0, sin( min( 1.0, delta_Ma/delta_Ma_limit )*0.5*pi ) );
+    double phi = max( 0.0, pow( sin( min( 1.0, delta_Ma/delta_Ma_limit )*0.5*pi ), 2.0 ) );
 
     /// Conservative + (weighted) dissipative flux
     double F = 0.5*( F_L + F_R ) - 0.5*phi*S*( U_R - U_L );
