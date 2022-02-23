efc4b40d6d240ef10c90db13dd6cd09edd383b08
Lluis Jofre Cruanyes <lluis.jofre@upc.edu>
2022-02-23 18:48:03 +0100

    KGP+ scheme modified XVII

--- src/FlowSolverRHEA.cpp
+++ src/FlowSolverRHEA.cpp
@@ -2473,8 +2473,7 @@ double KgpPlusFluxApproximateRiemannSolver::calculateIntercellFlux(const double 
 
     /// Calculate dissipative weight
     double relative_sos_difference = abs( ( a_R - a_L )/( 0.5*( a_R + a_L + epsilon ) ) );
-    //double dissipative_weight      = max( 0.0, sin( min( relative_sos_difference, 1.0 )*0.5*pi ) );
-    double dissipative_weight      = max( 0.0, pow( sin( min( relative_sos_difference, 1.0 )*0.5*pi ), 0.5 ) );
+    double dissipative_weight      = max( 0.0, sin( min( relative_sos_difference, 1.0 )*0.5*pi ) );
 
     double U_L = rho_L;
     double U_R = rho_R;
