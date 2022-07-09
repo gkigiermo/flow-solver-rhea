60ffbeacd1b53f129925e26cabae6b84e46ccb8e
lluis.jofre <lluis.jofre@upc.upc.edu>
2022-07-09 12:31:49 +0200

    New function created calculateTemperatureFromPressureDensityWithInitialGuess III

--- src/ThermodynamicModel.cpp
+++ src/ThermodynamicModel.cpp
@@ -483,7 +483,8 @@ double PengRobinsonModel::calculateTemperatureFromPressureDensity(const double &
         x_2 = ( (bar_v - eos_b )/R_universal )*( P + ( this->calculate_eos_a( x_1 )/( pow( bar_v, 2.0 ) + 2.0*eos_b*bar_v - pow( eos_b, 2.0 ) ) ) );
 
         denominator = x_2 - 2.0*x_1 + x_0;
-        T = x_2 - ( pow( x_2 - x_1, 2.0 ) )/denominator;
+        //T = x_2 - ( pow( x_2 - x_1, 2.0 ) )/denominator;
+        T = x_2 - ( pow( x_2 - x_1, 2.0 ) )/( denominator + 1.0e-15 );
     
         if( abs( ( T - x_2 )/ T ) < aitken_relative_tolerance ) break;	/// If the result is within tolerance, leave the loop!
         x_0 = T;							/// Otherwise, update x_0 to iterate again ...                 
