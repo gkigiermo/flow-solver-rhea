2c14356ae96944d211fe1af648717dd90e65afa2
Lluis Jofre <lluis.jofre@upc.edu>
2023-04-11 19:43:18 +0200

    RootFindingMinimization improved II

--- src/ThermodynamicModel.hpp
+++ src/ThermodynamicModel.hpp
@@ -384,11 +384,11 @@ class PengRobinsonModel : public BaseThermodynamicModel {
 
         /// Aitken's delta-squared process parameters
         int max_aitken_iter              = 1000;		/// Maximum number of iterations
-        double aitken_relative_tolerance = 1.0e-5;		/// Relative tolerance
+        double aitken_relative_tolerance = 1.0e-8;		/// Relative tolerance
 
         /// Newton-Raphson solver parameters
         int max_nr_iter              = 1000;			/// Maximum number of iterations
-        double nr_relative_tolerance = 1.0e-5;			/// Relative tolerance
+        double nr_relative_tolerance = 1.0e-8;			/// Relative tolerance
         NR_P_T_from_rho_e *nr_PT_solver;			/// Pointer to NR_P_T_from_rho_e
 	std::vector<double> nr_PT_unknowns;			/// NR_P_T_from_rho_e unknowns: P & T
 	std::vector<double> nr_PT_r_vec;			/// NR_P_T_from_rho_e vector of functions residuals
